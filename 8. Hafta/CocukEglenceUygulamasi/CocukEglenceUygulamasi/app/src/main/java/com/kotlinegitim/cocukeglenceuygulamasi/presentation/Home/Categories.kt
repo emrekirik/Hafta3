f360ae6f9219c3ce8c083a9a2c674ec4ebb1784c
Emre <63380855+emrekirik@users.noreply.github.com>
2023-09-18 15:00:50 +0300

    Delete 8. Hafta/CocukEglenceUygulamasi/CocukEglenceUygulamasi directory

--- 8. Hafta/CocukEglenceUygulamasi/CocukEglenceUygulamasi/app/src/main/java/com/kotlinegitim/cocukeglenceuygulamasi/presentation/Home/Categories.kt
+++
@@ -1,8 +0,0 @@
-package com.kotlinegitim.cocukeglenceuygulamasi.presentation.Home
-
-import com.google.firebase.database.IgnoreExtraProperties
-import java.io.Serializable
-
-@IgnoreExtraProperties
-data class Categories(var categoryName:String?=""): Serializable {
-}
